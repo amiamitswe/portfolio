70eab5e72aa7dd00ffd4c515d566b0f85226fd3c
Amit Samadder <amiamitswe@gmail.com>
2024-03-17 21:05:27 +0600

    test

--- src/components/MySkills.jsx
+++ src/components/MySkills.jsx
@@ -14,7 +14,7 @@ function MySkills() {
   return (
     <div className="max-w-7xl w-full mx-auto p-6 lg:px-8 lg:mt-[200px] mt-28">
       <SectionTitle title="My Skills" info="My expertise in technology" />
-      <div className="lg:w-10/12 w-full mx-auto">
+      <div className="lg:w-10/12 w-full mx-auto ">
         {skills?.map((skill) => (
           <Skill
             key={skill.title}
